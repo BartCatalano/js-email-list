b7a29a676c31b49ef04ea57c148f3d466968ec8a
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-18 15:51:32 +0100

    feat: pagina sistemata con bootstrap, miglioramento grafico (WIP)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const listamail = document.getElementById("lista-mail") 
 // PRENDO IL BOTTONE PER GENERARE LE MAIL
 const btnMail = document.getElementById("btn-mail");
+const titoloLista = document.getElementById("titolo");
 
 
 
@@ -18,7 +19,9 @@ const casualMail = [];
 btnMail.addEventListener('click', function() {
 for (let i = 0; i < 10; i++){
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) =>{
-        listamail.innerHTML +=`<li>${resp.data.response}</li>`
+        listamail.innerHTML +=`<li class="list-group-item">${resp.data.response}</li>`
+        btnMail.disabled = true;
+        titoloLista.innerHTML = "Lista delle mail generata casualmente";
     });
 };
   
